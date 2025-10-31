558987c8e1321fa944c8c1138a1ebfb2a7207b0c
George Tarantilis <gtaranti@gmail.com>
2025-10-31 17:11:30 +0200

    end of chapter 5

--- lib/tunez/accounts/user.ex
+++ lib/tunez/accounts/user.ex
@@ -4,7 +4,7 @@ defmodule Tunez.Accounts.User do
     domain: Tunez.Accounts,
     data_layer: AshPostgres.DataLayer,
     authorizers: [Ash.Policy.Authorizer],
-    extensions: [AshAuthentication]
+    extensions: [AshJsonApi.Resource, AshAuthentication]
 
   authentication do
     add_ons do
@@ -44,6 +44,10 @@ defmodule Tunez.Accounts.User do
         end
       end
     end
+  end
+
+  json_api do
+    type "user"
   end
 
   postgres do
