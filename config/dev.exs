ebf47a274300079f9bd4228f1ce16a984c9a3f02
George Tarantilis <gtaranti@gmail.com>
2025-10-31 13:25:10 +0200

    after 'mix igniter.install ash_authentication'

--- config/dev.exs
+++ config/dev.exs
@@ -64,7 +64,7 @@ config :tunez, TunezWeb.Endpoint,
   ]
 
 # Enable dev routes for dashboard and mailbox
-config :tunez, dev_routes: true
+config :tunez, dev_routes: true, token_signing_secret: "bfAdb9+D9YhdPZ2I+buowVHupgGUuPcK"
 
 # Do not include metadata nor timestamps in development logs
 config :logger, :default_formatter, format: "[$level] $message\n"
--- config/test.exs
+++ config/test.exs
@@ -1,4 +1,6 @@
 import Config
+config :tunez, token_signing_secret: "iR813rVHA3Gge3FpXxbOT/JEfo/H2XNS"
+config :bcrypt_elixir, log_rounds: 1
 config :ash, policies: [show_policy_breakdowns?: true], disable_async?: true
 
 # Configure your database
