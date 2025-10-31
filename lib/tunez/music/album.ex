6f4f6ea2a7eedde7c5718f48764219dfd046f546
George Tarantilis <gtaranti@gmail.com>
2025-10-31 11:25:31 +0200

    end of chapter 4

--- lib/tunez/music/album.ex
+++ lib/tunez/music/album.ex
@@ -3,7 +3,11 @@ defmodule Tunez.Music.Album do
     otp_app: :tunez,
     domain: Tunez.Music,
     data_layer: AshPostgres.DataLayer,
-    extensions: [AshJsonApi.Resource]
+    extensions: [AshGraphql.Resource, AshJsonApi.Resource]
+
+  graphql do
+    type :album
+  end
 
   json_api do
     type "album"
@@ -70,6 +74,8 @@ defmodule Tunez.Music.Album do
     end
   end
 
+  def next_year, do: Date.utc_today().year + 1
+
   calculations do
     calculate :years_ago, :integer, expr(2025 - year_released)
 
@@ -77,8 +83,6 @@ defmodule Tunez.Music.Album do
               :string,
               expr("wow, this was released " <> years_ago <> " years ago!")
   end
-
-  def next_year, do: Date.utc_today().year + 1
 
   identities do
     identity :unique_album_names_per_artist, [:name, :artist_id],
