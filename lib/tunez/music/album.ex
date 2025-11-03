b02fb8deed2aa2315314d546c21282361b33d1e5
George Tarantilis <gtaranti@gmail.com>
2025-11-03 11:29:53 +0200

    page 203

--- lib/tunez/music/album.ex
+++ lib/tunez/music/album.ex
@@ -28,10 +28,15 @@ defmodule Tunez.Music.Album do
 
     create :create do
       accept [:name, :year_released, :cover_image_url, :artist_id]
+      argument :tracks, {:array, :map}
+      change manage_relationship(:tracks, type: :direct_control, order_is_key: :order)
     end
 
     update :update do
       accept [:name, :year_released, :cover_image_url]
+      require_atomic? false
+      argument :tracks, {:array, :map}
+      change manage_relationship(:tracks, type: :direct_control, order_is_key: :order)
     end
   end
 
@@ -101,6 +106,10 @@ defmodule Tunez.Music.Album do
 
     belongs_to :created_by, Tunez.Accounts.User
     belongs_to :updated_by, Tunez.Accounts.User
+
+    has_many :tracks, Tunez.Music.Track do
+      sort order: :asc
+    end
   end
 
   calculations do
@@ -109,6 +118,14 @@ defmodule Tunez.Music.Album do
     calculate :string_years_ago,
               :string,
               expr("wow, this was released " <> years_ago <> " years ago!")
+  end
+
+  calculations do
+    calculate :duration, :string, Tunez.Music.Calculations.SecondsToMinutes
+  end
+
+  aggregates do
+    sum :duration_seconds, :tracks, :duration_seconds
   end
 
   identities do
